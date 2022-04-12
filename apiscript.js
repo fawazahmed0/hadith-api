5ef55f42017088bbe401d1174b913cc911fa589e
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-12 23:57:51 +0530

    Update apiscript.js

--- apiscript.js
+++ apiscript.js
@@ -9,6 +9,9 @@ const {
     firefox
   } = require('playwright');
 
+  // Flags
+// whether to check for duplicate translation or not during create
+var checkduplicate = true;
 // whether json is required in translation or not
 var jsonrequired = true
 // Folder that holds all the translations that needs to be added
@@ -51,7 +54,7 @@ var browser
 
 // function that will run on running this script
 async function start() {
-    logmsg("\nBEGIN:\n" + process.argv.join(' '), true)
+    util.logmsg("\nBEGIN:\n" + process.argv.join(' '), true)
     // Print the help and how to use the script file and arguments, same as given in contribute
     if (argarr[0] == undefined)
       helpPrint()
@@ -65,8 +68,7 @@ async function start() {
       // storing the data in jsondb, so editionsListingsGen function can work and create editions.json
       await jsonDB()
       deleteEditions(argarr.slice(1))
-    } else if (argarr[0].toLowerCase().trim() == "fontsgen")
-      fontsGen()
+    } 
     else
       helpPrint()
   
@@ -84,11 +86,10 @@ function helpPrint() {
     console.log("\nupdate\nupdates the database, copy the edition that needs to be edited from database/chapterverse directory and paste that edition in start directory and then perform any editing you want in the file and then run this command\nExample: node ", filename, " update")
     console.log("\ndelete\ndeletes the edition from database\nExample: node ", filename, " delete editionNameToDelete")
     console.log("\nsearch\nsearches the provided line in database\nExample: node ", filename, ' search "verseToSearch"')
-    console.log("\nfontsgen\ngenerates the fonts, paste your fonts in start direcotry and then run this command\nExample: node ", filename, ' fontsgen')
   }
 
 
-  async function create(){
+  async function create(update){
  // saving database snippet, filename and it's json data in jsondb variable
  await jsonDB()
  // saving isocodes in json
@@ -105,25 +106,167 @@ function helpPrint() {
   // we don't want to read .gitkeep, it is used as a placeholder for start direcotory to exist in git
   if (filename == '.gitkeep')
     continue;
-    logmsg("\nStarting to create files for " + filename)
+    util.logmsg("\nStarting to create files for " + filename)
     // Reading the file and retrieving as array, filteredarr, and jsondata inside it
     // filterarr doesn't contain jsondata and empty lines in it
-    var [orgarr, cleanarr, jsondata] = readDBTxt(path.join(startDir, filename))
+    var [orgarr, cleanarr, jsondata] = util.readDBTxt(path.join(startDir, filename))
 
     if (!jsondata) {
-      logmsg("\nNo JSON found in file " + filename + " or please enter the json in correct format", true)
+      util.logmsg("\nNo JSON found in file " + filename + " or please enter the json in correct format", true)
       jsondata = {}
       if (jsonrequired) {
         var tempjson = '{"author":"Name of Author","language":"Name of language","source":"","comments":""}'
-        logmsg("\nAdd json at end of file in the following format:\n\n" + JSON.stringify(JSON.parse(tempjson), null, prettyindent))
+        util.logmsg("\nAdd json at end of file in the following format:\n\n" + JSON.stringify(JSON.parse(tempjson), null, prettyindent))
         continue
       }
 
     }
 
+        // Now we have to check and make sure same copy doesn't exists in the repo, here we will use the linebylineDir to check
+        var duplicatefilename = checkduplicateTrans(cleanarr)
+        // We don't want to check for duplicates during update operation
+        if (duplicatefilename && !update) {
+          util.logmsg("\nThis file " + filename + " seems to be a duplicate copy of edition " + duplicatefilename.replace(/(\.[^\.]*$)/i, ""))
+          if (checkduplicate)
+            continue
+          else
+            util.logmsg("\ncheckduplicate is set to false, so a duplicate copy of this translation will be created in the database")
+        }
+
+        var temp = isoLangMap([jsondata['language']])
+        // if the above fails, then we will have to detect the language
+        if (!Array.isArray(temp)){
+        util.logmsg("\nPlease specify the proper iso name of the language in json, Skipping this translation " + filename)
+        continue;
+        }
+              // Assigning isoname of the language and it's isocode
+      jsondata['language'] = temp[0]
+      jsondata['iso'] = temp[1]
+
+      if (update) {
+
+        if (!fs.existsSync(path.join(linebylineDir, filename))) {
+          util.logmsg("\nEdition with name " + filename.replace(/(\.[^\.]*$)/i, "") + " does not exist in the database")
+          continue
+        } else if (jsondata['name'] && filename.replace(/(\.[^\.]*$)/i, "") != jsondata['name']) {
+          util.logmsg("\nYou are trying to change edition name to " + jsondata['name'] + " this should be done only in very rare cases only")
+          if (fs.existsSync(path.join(linebylineDir, jsondata['name'] + '.txt'))) {
+            util.logmsg("\nA Edition with same name as " + jsondata['name'] + " exists in the database, you will have to give a new edition name")
+            continue
+          }
+        }
+  
+        // delete the old editions
+        var oldEditionName = filename.replace(/(\.[^\.]*$)/i, "")
+        deleteEditions([oldEditionName])
+      }
+
+
+        // generating edition
+    util.logmsg("\nGenerating Edition")
+    // if edition name exists in the file during update process, we will use that as edition name
+    if (update && jsondata['name'])
+      var genJSON = await generateEdition(cleanarr, jsondata, jsondata['name'])
+    else
+      var genJSON = await generateEdition(cleanarr, jsondata)
+
+    if (update) {
+      // if this is update operation, then we will give more preference to json data from file, instead of generated json data
+      for (var [key, val] of Object.entries(genJSON)) {
+        // we want the autogenerated link, linkmin and direction values
+        if (jsondata[key] && !key.includes('link') && key != 'direction')
+          genJSON[key] = jsondata[key]
+      }
+    }
+        // move the file for which update/create have been completed from startDir to originals dir
+        fs.renameSync(path.join(startDir, filename), path.join(databaseDir, "originals", filename))
+
+        
+      }
+        // Generate the editions.json
+  editionsListingsGen()
+  // close the browser when everything is done
+  await browser.close();
+
+    
+
+    
+  
+
  }
     
+
+ // This function is a wrapper to generate json and generate the files in the database
+async function generateEdition(arr, jsondata, editionName) {
+  var genJSON = {}
+  // use the editionName to generateJSON if it's defined
+  if (editionName)
+    genJSON = await generateJSON(arr, jsondata, editionName)
+  else
+    genJSON = await generateJSON(arr, jsondata)
+
+  // generate files in database
+  generateFiles(arr, genJSON)
+  // save the json data and snippet inside the jsondb variable
+  await jsonDB(genJSON['name'] + '.txt')
+  util.logmsg("\n Generated edition " + genJSON['name'])
+
+  return genJSON
+}
+
+
+// function to delete list of editions from the database
+// This will also remove the auto generated -la and -lad of edition
+function deleteEditions(arr) {
+  var deleted = false
+  for (var val of arr) {
+    for (var editionname of [val, val + '-la', val + '-lad']) {
+      // array containing paths to delete
+      var pathsarr = []
+      pathsarr.push(path.join(editionsDir, editionname))
+      pathsarr.push(path.join(editionsDir, editionname + '.json'))
+      pathsarr.push(path.join(editionsDir, editionname + '.min.json'))
+      pathsarr.push(path.join(linebylineDir, editionname + '.txt'))
+      pathsarr.push(path.join(databaseDir, 'chapterverse', editionname + '.txt'))
+
+      for (var pathToDelete of pathsarr) {
+        if (fs.existsSync(pathToDelete)) {
+          deleted = true
+          if (fs.statSync(pathToDelete).isDirectory())
+            fs.rmdirSync(pathToDelete, {
+              recursive: true
+            })
+          else
+            fs.unlinkSync(pathToDelete)
+        }
+      }
+      // Deleting also from temporary jsondb variable
+      delete jsondb[editionname + '.txt']
+      util.logmsg("\n deletion completed for " + editionname)
+    }
   }
+  // Generate the editions.json if any of the file was deleted
+  if (deleted)
+    editionsListingsGen()
+}
+
+// reads the jsondb variable to generate editions.json
+function editionsListingsGen() {
+  var newjsondb = {}
+  // we will always keep the editions.json in sorted order, so it's easier to find
+  var sortedkeys = Object.keys(jsondb).sort()
+  for (var name of sortedkeys) {
+    // removing .txt from filename and replace dash with underscore as many programming languages doesn't support - (dash) in json object key
+    var newname = name.replace(/\..*/gi, "").replace(/-/gi, "_")
+    newjsondb[newname] = jsondb[name]['jsondata']
+  }
+
+  fs.writeFileSync(editionsDir + ".json", JSON.stringify(newjsondb, null, prettyindent))
+  fs.writeFileSync(editionsDir + ".min.json", JSON.stringify(newjsondb))
+  util.logmsg("\neditions.json and editions.min.json generated")
+}
+
+  
 
     // Stores the translation files snippets and it's json,retreieves them from linebylineDir
 async function jsonDB(singlefile) {
@@ -174,4 +317,63 @@ async function launchBrowser(linkToOpen, downloadPathDir) {
     await page.goto(linkToOpen, {
       timeout: 60000
     });
+}
+
+// Generates the json with standard naming conventions
+async function generateJSON(arr, newjson, editionName) {
+
+  var isocode = newjson['iso']
+  // Deleting iso key, as it might create a bug in the future, as this key was added later to solve an issue in actions enviroment
+  delete newjson['iso']
+  // capitalize first letters
+  newjson['language'] = capitalize(newjson['language'])
+  // If values are undefined we will assign it as empty string
+  newjson['author'] = newjson['author'] || "unknown"
+
+  // Removing special symbols and diacritics from authors name
+  newjson['author'] = newjson['author'].normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^A-Za-z\s\.\,]+/gi, " ").replace(/\s\s+/gi, " ").toLowerCase().trim()
+  newjson['author'] = capitalize(newjson['author'])
+
+  // If values are undefined we will assign it as empty string
+  newjson['source'] = newjson['source'] || ""
+  newjson['comments'] = newjson['comments'] || ""
+
+
+  // Number of chars to consider in author name for editionName creation
+  var authorSize = 15
+  // Take first few chars of like 10chars for author to make editionName
+  // editionName will be a foldername and also part of url, so cannot have anything other than latin alphabets
+  if (!editionName)
+    editionName = isocode + "-" + newjson['author'].toLowerCase().replace(/[^A-Za-z]+/gi, "").substring(0, authorSize);
+
+  // first check file with same endpoint exists or not in editions.json, if there then we will add 1 to the editionname and check again
+  for (var i = 1;; i++) {
+    // If a filename with same edition name exists in database then add number to the editionName
+    if (jsondb[editionName + '.txt'] || jsondb[editionName + '-la.txt'] || jsondb[editionName + '-lad.txt']) {
+      // Fetch the number if exists in the editionName
+      var Num = editionName.match(/\d+$/) || [0]
+      Num = parseInt(Num[0])
+      // Increment that number if it exists to get a new editionName
+      editionName = editionName.replace(/\d+$/, "") + (Num + 1);
+    } else
+      break;
+  }
+
+  newjson['name'] = editionName
+  newjson['link'] = url + editionsFolder + "/" + editionName + ".json"
+  newjson['linkmin'] = url + editionsFolder + "/" + editionName + ".min.json"
+  newjson['direction'] = await dirCheck(arr.slice(0, 10).join('\n'))
+
+  // JSON in sorted order
+  var sortjson = {}
+  sortjson['name'] = newjson['name']
+  sortjson['author'] = newjson['author']
+  sortjson['language'] = newjson['language']
+  sortjson['direction'] = newjson['direction']
+  sortjson['source'] = newjson['source']
+  sortjson['comments'] = newjson['comments']
+  sortjson['link'] = newjson['link']
+  sortjson['linkmin'] = newjson['linkmin']
+
+  return sortjson
 }

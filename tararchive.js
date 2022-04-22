206381844a14e1add040558019026a793f265184
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-23 05:07:56 +0530

    Update tararchive.js

--- tararchive.js
+++ tararchive.js
@@ -1,8 +1,7 @@
 const tar = require('tar');
 const fs = require('fs');
 const path = require('path');
-const util = require('util')
-const exec = util.promisify(require('child_process').exec)
+const exec = require('child_process').execSync;
 
 let filesArr = [];
 let tarballName = 'mytarball.tar'
@@ -59,7 +58,7 @@ const getEntryFilenames = async tarballFilename => {
       let toAddFiles = cleanFilesArr.map(e=>tarballOptions.prefix+'/'+e.split(path.sep).join('/'))
       let toDeleteFiles = toAddFiles.filter(e=>tarballFiles.includes(e))
       // take n files at a time to avoid exec issues
-      let batchArr = TwoDimensional(toDeleteFiles, 1000) 
+      let batchArr = TwoDimensional(toDeleteFiles, 300) 
       for(let batch of batchArr)
       await exec('tar --delete --file='+tarballName+' '+batch.join(' '), { maxBuffer: Infinity })
       

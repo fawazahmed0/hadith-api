11c10a69ac9364afe210674c423717241aacd13c
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-23 03:51:05 +0530

    add tar update

--- tararchive.js
+++ tararchive.js
@@ -1,23 +1,46 @@
 const tar = require('tar');
 const fs = require('fs');
 const path = require('path');
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 
 let filesArr = [];
 let tarballName = 'mytarball.tar'
+let tarballPath = path.join(__dirname,tarballName)
 let foldersToExclude = ['node_modules','.git','originals','start',tarballName,tarballName+'.gz']
+let tarballOptions =  {file: tarballName, prefix: 'dir'}
+let update = false
+
+const getEntryFilenames = async tarballFilename => {
+  const filenames = []
+  await tar.t({
+    file: tarballFilename,
+    onentry: entry => filenames.push(entry.path),
+  })
+  return filenames
+}
   async function test(){
     traverseDir('.',foldersToExclude)
     let filesObj = {}
     for (let value of filesArr)
     filesObj[value] = fs.statSync(value).size
     filesObj= sortJSONbyValue(filesObj)
+
+    if(fs.existsSync(tarballPath))
+    update=true
+ 
     //compression ratio for 7zip
     let compressionRatio = 12
     let sizeLimit = 195 * 1000000 //195MB
     sizeLimit*=compressionRatio
 
 
+
     let cleanFilesArr=[],sum=0;
+
+    if(update)
+    sum+=fs.statSync(tarballPath).size
+
     for (let [key,value] of Object.entries(filesObj)) {
         sum+=value
         if(sum>sizeLimit)
@@ -27,15 +50,24 @@ let foldersToExclude = ['node_modules','.git','originals','start',tarballName,ta
     }
 
 
+   //update tar
+    if(update){
+      // get tarball files list
+      let tarballFiles = await getEntryFilenames(tarballPath)
 
-    //create tar
-    await tar.c(
-        {
-          file: tarballName,
-          prefix: 'dir',
-        },
-        cleanFilesArr
-      )
+       // delete files from tarball
+      let toAddFiles = cleanFilesArr.map(e=>tarballOptions.prefix+'/'+e.split(path.sep).join('/'))
+      let toDeleteFiles = toAddFiles.filter(e=>tarballFiles.includes(e))
+      await exec('tar --delete --file='+tarballName+' '+toDeleteFiles.join(' '), { maxBuffer: Infinity })
+      
+      // now update tarball with new files
+      await tar.r(tarballOptions,cleanFilesArr)
+    }
+    //create tarball
+      else
+      await tar.c(tarballOptions, cleanFilesArr)
+
+
 
   }
 
@@ -63,3 +95,4 @@ function sortJSONbyValue(json){
   let entries  =   Object.entries(json).sort((a,b) => b[1] -  a[1] )
   return Object.fromEntries(entries)
 }
+

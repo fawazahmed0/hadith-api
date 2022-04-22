98b9885d261019a12a3ef2ab50cdc24fe4445e38
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-22 16:13:43 +0530

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path')
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 
 

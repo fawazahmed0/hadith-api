edefb8ab20886cb35aaf495fcd55e65c83580b6b
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-23 18:02:26 +0530

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -1,3 +1,5 @@
+// contains different utility functions to make things easier
+
 const fs = require('fs');
 const path = require('path')
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
